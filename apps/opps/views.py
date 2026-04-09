bc4cbdf5719e96225b839291622e4e53334d5e4f
Jonathan Jackson <jjackson@dimagi.com>
2026-04-08 23:54:57 -0600

    fix(opps): remove unused settings import from views.py

--- apps/opps/views.py
+++ apps/opps/views.py
@@ -1,5 +1,4 @@
 """REST API views for the ACE opportunity Workbench."""
-from django.conf import settings
 from django.http import HttpResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
